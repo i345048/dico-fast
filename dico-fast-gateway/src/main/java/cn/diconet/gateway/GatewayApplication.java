4577a729d7868c01c9cecd5cd870ea5622eefa9f
271129083@qq.com <271129083@qq.com>
2018-04-06 14:48:44 +0800

    网关对微服务的认证

--- dico-fast-gateway/src/main/java/cn/diconet/gateway/GatewayApplication.java
+++ dico-fast-gateway/src/main/java/cn/diconet/gateway/GatewayApplication.java
@@ -3,6 +3,7 @@ package cn.diconet.gateway;
 import cn.diconet.gateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
