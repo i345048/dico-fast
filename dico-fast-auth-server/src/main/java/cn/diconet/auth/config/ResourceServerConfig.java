8c5f049efac9790c42b6bb9b03782dfa33c67cea
tangzan <271129083@qq.com>
2018-04-11 17:56:51 +0800

    客户端登录统一的login成功后, 客户端其他页面鉴权方式待解决

--- dico-fast-auth-server/src/main/java/cn/diconet/auth/config/ResourceServerConfig.java
+++ dico-fast-auth-server/src/main/java/cn/diconet/auth/config/ResourceServerConfig.java
@@ -1,9 +1,16 @@
 package cn.diconet.auth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,6 +22,9 @@ import javax.servlet.http.HttpServletResponse;
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private RedisConnectionFactory connectionFactory;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -29,5 +39,23 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
             .and()
             .httpBasic();
             // .formLogin().permitAll();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenServices(tokenServices());
+    }
+
+    @Bean
+    public RedisTokenStore tokenStore() {
+        return new RedisTokenStore(connectionFactory);
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        return defaultTokenServices;
     }
 }
